bee3dc46a9f104184457369e9b73132fda5d67a6
Eden Denis F. da S. L. Santos <49501168+edendenis@users.noreply.github.com>
2024-06-27 15:26:04 -0300

    Updating folders and files
    Updating folders and files.

--- README.py
+++ README.py
@@ -1,93 +1,158 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Como instalar/configurar o `Tex Live` no `Linux Ubuntu`
+# # Como configurar/instalar/usar o `LaTeX OCR` no `Linux Ubuntu`
 # 
 # ## Resumo
 # 
-# Neste documento estão contidos os principais comandos e configurações para configurar/instalar o `Tex Live` no `Linux Ubuntu`.
+# Neste documento estão contidos os principais comandos e configurações para configurar/instalar/usar o `LaTeX OCR` no `Linux Ubuntu`.
 # 
 # ## _Abstract_
 # 
-# _This document contains the main commands and configurations for configuring/installing `Tex Live` on `Linux Ubuntu`._
+# _This document contains the main commands and settings to install/configure/use to `LaTeX OCR` on `Linux Ubuntu`._
 
-# ### Construído com
+# ## Descrição [1]
 # 
-# Esta seção deve listar todas as principais estruturas/bibliotecas usadas para inicializar seu projeto, bem como a sequência de instalação. Deixe quaisquer complementos/plugins para a seção de agradecimentos. Aqui estão alguns exemplos.
+# ### `LaTeX OCR`
 # 
-# * [![Texlive](https://img.shields.io/badge/Texlive-3776AB?style=flat-square&logo=latex&logoColor=white)](https://tug.org/texlive/)
-# * [![JabRef](https://img.shields.io/badge/JabRef-44A833?style=flat-square&logo=latex&logoColor=white)](https://www.jabref.org/)
-# * [![Texstudio](https://img.shields.io/badge/Texstudio-008080?style=flat-square&logo=latex&logoColor=white)](https://www.texstudio.org/)
-# * [![MathPix](https://img.shields.io/badge/MathPix-008080?style=flat-square&logo=MathPix&logoColor=white)](https://mathpix.com/)
-# 
-# <p align="right">(<a href="#readme-top">voltar ao topo</a>)</p>
+# O `LaTeX OCR` (Reconhecimento Óptico de Caracteres para LaTeX) é uma ferramenta que converte imagens de texto matemático ou científico em código `LaTeX`. Isso é particularmente útil para pesquisadores, engenheiros, cientistas e estudantes que trabalham com documentos que contêm muitas equações matemáticas e querem convertê-las para um formato digital editável sem ter que digitar manualmente o código `LaTeX`.
 # 
 
-# ## Descrição [2]
+# ## 1. Configurar/Instalar/Usar o `PyTorch` no `Linux` Ubuntu (caso NÂO tenha sido instalado) [1][2]
 # 
-# ### `Tex Live`
-# 
-# O TeX Live é uma distribuição abrangente e amplamente utilizada do sistema de composição de documentos TeX, desenvolvido por Donald Knuth. Ele inclui uma vasta coleção de pacotes, fontes e utilitários relacionados ao TeX, tornando-o uma escolha popular entre autores e profissionais que precisam criar documentos de alta qualidade, como artigos acadêmicos, livros, relatórios técnicos e documentos matemáticos. O TeX Live está disponível em várias plataformas, incluindo Windows, macOS e Linux, e oferece suporte a uma variedade de idiomas e scripts. É uma ferramenta essencial para aqueles que desejam produzir documentos tipograficamente precisos e bem formatados.
-
-# ## 1. Configurar/Instalar/Usar o `Tex Live` no `Linux Ubuntu` [1]
-# 
-# Você pode instalar o `Tex Live` no `Linux Ubuntu` usando o gerenciador de pacotes `apt`. Aqui está um guia passo a passo:
+# Para executar o modelo você precisa do `Python 3.7+`. Se você não tiver o `PyTorch` instalado. Siga as instruções abaixo:
 # 
 # 1. Abra o `Terminal Emulator`. Você pode fazer isso pressionando: `Ctrl + Alt + T`
 # 
 
 # 2. Certifique-se de que seu sistema esteja limpo e atualizado.
 # 
-#     2.1 Remover pacotes `.deb` antigos ou duplicados do cache local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
+#     2.1 Limpar o `cache` do gerenciador de pacotes `apt`. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
+#     
+#     2.2 Remover pacotes `.deb` antigos ou duplicados do cache local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
 # 
-#     2.2 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando: `sudo apt autoremove -y`
+#     2.3 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando: `sudo apt autoremove -y`
 # 
-#     2.3 Buscar as atualizações disponíveis para os pacotes que estão instalados em seu sistema. Digite o seguinte comando e pressione `Enter`: `sudo apt update -y`
+#     2.4 Buscar as atualizações disponíveis para os pacotes que estão instalados em seu sistema. Digite o seguinte comando e pressione `Enter`: `sudo apt update`
 # 
-#     2.4 Para ver a lista de pacotes a serem atualizados, digite o seguinte comando e pressione `Enter`:  `sudo apt list --upgradable`
+#     2.5 **Corrigir pacotes quebrados**: Isso atualizará a lista de pacotes disponíveis e tentará corrigir pacotes quebrados ou com dependências ausentes: `sudo apt --fix-broken install`
 # 
-#     2.5 Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update -y`. Digite o seguinte comando e pressione `Enter`: `sudo apt full-upgrade -y`
+#     2.6 Limpar o `cache` do gerenciador de pacotes `apt`. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
+#     
+#     2.7 Para ver a lista de pacotes a serem atualizados, digite o seguinte comando e pressione `Enter`:  `sudo apt list --upgradable`
 # 
-#     2.6 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando: `sudo apt autoremove -y`
-# 
-#     2.7 Remover pacotes `.deb` antigos ou duplicados do cache local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
+#     2.8 Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update`. Digite o seguinte comando e pressione `Enter`: `sudo apt full-upgrade -y`
+#     
 
-# 3. **Instale o `Tex Live`:** Agora você pode instalar o Tex Live executando o seguinte comando: `sudo apt install texlive-full -y`
+# 3. Você pode instalar o `PyTorch` através do conda, que pode gerenciar as versões CUDA para você: `conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y`
 # 
-# - Isso instalará o `Tex Live` completo, que inclui uma ampla variedade de pacotes e bibliotecas para a criação de documentos LaTeX.
+#     - Aqui, `cudatoolkit=11.3` é um exemplo, e você deve escolher a versão que corresponde ao seu driver CUDA ou à versão que deseja usar.
 # 
-# 4. **Verifique a instalação:** Depois que a instalação estiver concluída, você pode verificar se o `Tex Live` foi instalado corretamente executando o seguinte comando: `tex --version`
+#     - Se você decidir atualizar o driver da NVIDIA, após a instalação, reinicie o sistema e verifique se o CUDA está funcionando corretamente executando nvidia-smi no terminal.
 # 
-# - Isso deve exibir informações sobre a versão do `Tex Live` instalada.
+# 5. Para verificar novamente se o `PyTorch` pode acessar a CUDA após resolver os problemas do driver ou reinstalar uma versão compatível do `PyTorch`, você pode executar o mesmo comando Python:
 # 
-# Agora você tem o `Tex Live` instalado no seu sistema Ubuntu e pode começar a criar documentos LaTeX. Lembre-se de usar um editor LaTeX, como o TeXworks, para criar e compilar seus documentos.
+#     ```
+#     import torch
+#     print(torch.cuda.is_available())
+#     ```
+# 
+# Espera-se que, depois de seguir esses passos, o `torch.cuda.is_available()` retorne `True`, indicando que o `PyTorch` agora pode acessar a GPU.
 # 
 
-# ## 1.1 Código completo para configur/instalar
+# ## 2. Configurar/Instalar o `LaTeX OCR` no `Linux Ubuntu` (caso NÃO tenha sido instalado) [1][2]
 # 
-# Para instalar o `Tex Live` no `Linux Ubuntu` sem precisar digitar linha por linha, você pode seguir estas etapas:
+# Para instalar o `LaTeX OCR`. Siga as instruções abaixo:
+# 
+# 1. Abra o `Terminal Emulator`. Você pode fazer isso pressionando: `Ctrl + Alt + T`
+# 
+
+# 2. Certifique-se de que seu sistema esteja limpo e atualizado.
+# 
+#     2.1 Limpar o `cache` do gerenciador de pacotes `apt`. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
+#     
+#     2.2 Remover pacotes `.deb` antigos ou duplicados do cache local. É útil para liberar espaço, pois remove apenas os pacotes que não podem mais ser baixados (ou seja, versões antigas de pacotes que foram atualizados). Digite o seguinte comando: `sudo apt autoclean`
+# 
+#     2.3 Remover pacotes que foram automaticamente instalados para satisfazer as dependências de outros pacotes e que não são mais necessários. Digite o seguinte comando: `sudo apt autoremove -y`
+# 
+#     2.4 Buscar as atualizações disponíveis para os pacotes que estão instalados em seu sistema. Digite o seguinte comando e pressione `Enter`: `sudo apt update`
+# 
+#     2.5 **Corrigir pacotes quebrados**: Isso atualizará a lista de pacotes disponíveis e tentará corrigir pacotes quebrados ou com dependências ausentes: `sudo apt --fix-broken install`
+# 
+#     2.6 Limpar o `cache` do gerenciador de pacotes `apt`. Especificamente, ele remove todos os arquivos de pacotes (`.deb`) baixados pelo `apt` e armazenados em `/var/cache/apt/archives/`. Digite o seguinte comando: `sudo apt clean` 
+#     
+#     2.7 Para ver a lista de pacotes a serem atualizados, digite o seguinte comando e pressione `Enter`:  `sudo apt list --upgradable`
+# 
+#     2.8 Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update`. Digite o seguinte comando e pressione `Enter`: `sudo apt full-upgrade -y`
+#     
+
+# 3. Instale o pacote `pix2tex`: `pip install "pix2tex[gui]"`
+# 
+#     Os pontos de verificação do modelo serão baixados automaticamente.
+#     
+# 4. Instale o pacote `xclip`, digite o comando: `sudo apt install xclip -y`
+# 
+# 5. Instale o pacote `wl-clipboard`: `sudo apt install wl-clipboard -y`
+# 
+#     Os pontos de verificação do modelo serão baixados automaticamente.
+# 
+
+# 6. Existem três maneiras de obter uma previsão de uma imagem.
+# 
+#     6.1 Você pode usar a ferramenta de linha de comando chamando `pix2tex`. Aqui você pode analisar imagens já existentes no disco e imagens na sua área de transferência ou executar o comando: `pix2tex </caminho/para/sua/imagem>.png`
+# 
+#     6.2 Graças a `@katie-lim`, você pode usar uma interface de usuário agradável como uma maneira rápida de obter a previsão do modelo. Basta chamar a GUI com `latexocr`. A partir daqui você pode fazer uma captura de tela e o código de latex previsto é renderizado usando MathJax e copiado para sua área de transferência.
+# 
+# - No Linux, é possível usar a GUI `gnome-screenshot` (que vem com suporte para vários monitores) se `gnome-screenshot` tiver sido instalada anteriormente. Para Wayland, `grim` e `slurp` será usado quando ambos estiverem disponíveis. Observe que `gnome-screenshot` não é compatível com compositores Wayland baseados em wlroots. Como `gnome-screenshot` será preferido quando disponível, talvez seja necessário definir a variável de ambiente `SCREENSHOT_TOOL` como `grim` neste caso (outros valores disponíveis são `gnome-screenshot` e `pil`).
+# 
+# - Se o modelo não tiver certeza sobre o que está na imagem, ele poderá gerar uma previsão diferente toda vez que você clicar em "Tentar novamente". Com o parâmetro `temperature` você pode controlar esse comportamento (baixa temperatura produzirá o mesmo resultado).
+# 
+
+# 6.3 Você pode usar uma API. Isso tem dependências adicionais. Instale `pip install -U "pix2tex[api]"` e execute: `python -m pix2tex.api.run`
+# 
+# Para iniciar uma demonstração Streamlit que se conecta à API na porta 8502. Há também uma imagem docker disponível para a API: https://hub.docker.com/r/lukasblecher/pix2tex Tamanho da imagem Docker (mais recente por data)
+# 
+# ```
+# docker pull lukasblecher/pix2tex:api
+# docker run --rm -p 8502:8502 lukasblecher/pix2tex:api
+# ```
+# 
+# Para executar também a demonstração `streamlit`, execute `docker run --rm -it -p 8501:8501 --entrypoint python lukasblecher/pix2tex:api pix2tex/api/run.py` e navegue até <http://localhost:8501/>
+# 
+
+# 6.4 Use de dentro do `Python`:
+# 
+# Para usar dentro do `Python`, utilize os comandos abaixo:
+# 
+# ```
+# from PIL import Image
+# from pix2tex.cli import LatexOCR
+# 
+# img = Image.open('path/to/image.png')
+# model = LatexOCR()
+# print(model(img))
+# ```
+# 
+# O modelo funciona melhor com imagens de resolução menor. É por isso que adicionei uma etapa de pré-processamento onde outra rede neural prevê a resolução ideal da imagem de entrada. Este modelo redimensionará automaticamente a imagem personalizada para melhor se assemelhar aos dados de treinamento e, assim, aumentar o desempenho das imagens encontradas em estado selvagem. Ainda assim, não é perfeito e pode não ser capaz de lidar com imagens enormes de maneira ideal, portanto, não aumente o zoom antes de tirar uma foto.
+# 
+# Sempre verifique o resultado com cuidado. Você pode tentar refazer a previsão com outra resolução se a resposta estiver errada.
+# 
+
+# ## 1.1 Código completo para configurar/instalar/usar
+# 
+# Para configurar/instalar/usar o `LaTeX OCR` no `Linux Ubuntu` sem precisar digitar linha por linha, você pode seguir estas etapas:
 # 
 # 1. Abra o `Terminal Emulator`. Você pode fazer isso pressionando: `Ctrl + Alt + T`
 # 
 # 2. Digite o seguinte comando e pressione `Enter`:
 # 
 #     ```
-#     sudo apt clean                                                            
-#     sudo apt autoclean
-#     sudo apt autoremove -y
-#     sudo apt update
-#     sudo apt --fix-broken install
-#     sudo apt clean
-#     sudo apt list --upgradable
-#     sudo apt full-upgrade -y
-#     sudo apt install texlive-full -y
-#     tex --version
+#     NÃO há.
 #     ```
-# 
 
 # ## Referências
 # 
-# [1] OPEN AI. ***Instalar o tex live no linux ubuntu.*** Disponível em: <https://chat.openai.com/c/5341e112-5d65-410b-bec5-00b9a61a6650> (texto adaptado). ChatGPT. Acessado em: 29/09/2023 18:56.
+# [1]BLECHER, L.. ***Latex-ocr.*** Disponível em: <https://github.com/lukas-blecher/LaTeX-OCR/blob/main/README.md>. Acessado em: 08/11/2023 08:41.
 # 
-# [2] OPEN AI. ***Vs code: editor popular.*** Disponível em: <https://chat.openai.com/c/b640a25d-f8e3-4922-8a3b-ed74a2657e42> (texto adaptado). ChatGPT. Acessado em: 14/11/2023 18:56.
+# [2] OPENAI. ***Instalar pytorch no ubuntu.*** Disponível em: <https://chat.openai.com/c/d24a5115-c7f9-48e1-9c40-f243bc017c08> (texto adaptado). Acessado em: 08/11/2023 08:49.
 # 
