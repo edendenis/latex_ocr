c7dd71f0b02897d53852cfec8078314fbffc36d2
Eden Denis <edendenis@gmail.com>
2024-07-01 22:09:53 -0300

    Removing OLD folder

--- README.py
+++ README.py
@@ -15,10 +15,10 @@
 # 
 # ### `LaTeX OCR`
 # 
-# O `LaTeX OCR` (Reconhecimento Óptico de Caracteres para LaTeX) é uma ferramenta que converte imagens de texto matemático ou científico em código `LaTeX`. Isso é particularmente útil para pesquisadores, engenheiros, cientistas e estudantes que trabalham com documentos que contêm muitas equações matemáticas e querem convertê-las para um formato digital editável sem ter que digitar manualmente o código `LaTeX`.
+# O `LaTeX OCR` (Reconhecimento Óptico de Caracteres para `LaTeX`) é uma ferramenta que converte imagens de texto matemático ou científico em código `LaTeX`. Isso é particularmente útil para pesquisadores, engenheiros, cientistas e estudantes que trabalham com documentos que contêm muitas equações matemáticas e querem convertê-las para um formato digital editável sem ter que digitar manualmente o código `LaTeX`.
 # 
 
-# ## 1. Configurar/Instalar/Usar o `PyTorch` no `Linux` Ubuntu (caso NÂO tenha sido instalado) [1][2]
+# ## 1. Configurar/Instalar/Usar o `PyTorch` no `Linux` Ubuntu (caso **NÃO** tenha sido instalado) [1][2]
 # 
 # Para executar o modelo você precisa do `Python 3.7+`. Se você não tiver o `PyTorch` instalado. Siga as instruções abaixo:
 # 
@@ -44,13 +44,13 @@
 #     2.8 Realmente atualizar os pacotes instalados para as suas versões mais recentes, com base na última vez que você executou `sudo apt update`. Digite o seguinte comando e pressione `Enter`: `sudo apt full-upgrade -y`
 #     
 
-# 3. Você pode instalar o `PyTorch` através do conda, que pode gerenciar as versões CUDA para você: `conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y`
+# 3. Você pode instalar o `PyTorch` através do `conda`, que pode gerenciar as versões `CUDA` para você: `conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y`
 # 
 #     - Aqui, `cudatoolkit=11.3` é um exemplo, e você deve escolher a versão que corresponde ao seu driver CUDA ou à versão que deseja usar.
 # 
-#     - Se você decidir atualizar o driver da NVIDIA, após a instalação, reinicie o sistema e verifique se o CUDA está funcionando corretamente executando nvidia-smi no terminal.
+#     - Se você decidir atualizar o driver da `NVIDIA`, após a instalação, reinicie o sistema e verifique se o `CUDA` está funcionando corretamente executando `nvidia-smi` no `Terminal Emulator`.
 # 
-# 5. Para verificar novamente se o `PyTorch` pode acessar a CUDA após resolver os problemas do driver ou reinstalar uma versão compatível do `PyTorch`, você pode executar o mesmo comando Python:
+# 5. Para verificar novamente se o `PyTorch` pode acessar a `CUDA` após resolver os problemas do driver ou reinstalar uma versão compatível do `PyTorch`, você pode executar o mesmo comando `Python`:
 # 
 #     ```
 #     import torch
@@ -60,7 +60,7 @@
 # Espera-se que, depois de seguir esses passos, o `torch.cuda.is_available()` retorne `True`, indicando que o `PyTorch` agora pode acessar a GPU.
 # 
 
-# ## 2. Configurar/Instalar o `LaTeX OCR` no `Linux Ubuntu` (caso NÃO tenha sido instalado) [1][2]
+# ## 2. Configurar/Instalar/Usar o `LaTeX OCR` no `Linux Ubuntu` (caso **NÃO** tenha sido instalado) [1][2]
 # 
 # Para instalar o `LaTeX OCR`. Siga as instruções abaixo:
 # 
@@ -97,20 +97,28 @@
 #     Os pontos de verificação do modelo serão baixados automaticamente.
 # 
 
-# 6. Existem três maneiras de obter uma previsão de uma imagem.
+# ## Usar o `LaTeX OCR`
 # 
-#     6.1 Você pode usar a ferramenta de linha de comando chamando `pix2tex`. Aqui você pode analisar imagens já existentes no disco e imagens na sua área de transferência ou executar o comando: `pix2tex </caminho/para/sua/imagem>.png`
+# Existem três maneiras de obter uma previsão de uma imagem.
 # 
-#     6.2 Graças a `@katie-lim`, você pode usar uma interface de usuário agradável como uma maneira rápida de obter a previsão do modelo. Basta chamar a GUI com `latexocr`. A partir daqui você pode fazer uma captura de tela e o código de latex previsto é renderizado usando MathJax e copiado para sua área de transferência.
+# ### Linha de comando
 # 
-# - No Linux, é possível usar a GUI `gnome-screenshot` (que vem com suporte para vários monitores) se `gnome-screenshot` tiver sido instalada anteriormente. Para Wayland, `grim` e `slurp` será usado quando ambos estiverem disponíveis. Observe que `gnome-screenshot` não é compatível com compositores Wayland baseados em wlroots. Como `gnome-screenshot` será preferido quando disponível, talvez seja necessário definir a variável de ambiente `SCREENSHOT_TOOL` como `grim` neste caso (outros valores disponíveis são `gnome-screenshot` e `pil`).
+# 1. Você pode usar a ferramenta de linha de comando chamando `pix2tex`. Aqui você pode analisar imagens já existentes no disco e imagens na sua área de transferência ou executar o comando: `pix2tex </caminho/para/sua/imagem>.png`
 # 
-# - Se o modelo não tiver certeza sobre o que está na imagem, ele poderá gerar uma previsão diferente toda vez que você clicar em "Tentar novamente". Com o parâmetro `temperature` você pode controlar esse comportamento (baixa temperatura produzirá o mesmo resultado).
+# #### Interface gráfica
+# 
+# 1. Graças a `@katie-lim`, você pode usar uma interface de usuário agradável como uma maneira rápida de obter a previsão do modelo. Basta chamar a GUI com `latexocr`. A partir daqui você pode fazer uma captura de tela e o código de latex previsto é renderizado usando MathJax e copiado para sua área de transferência.
+# 
+# - No `Linux`, é possível usar a GUI `gnome-screenshot` (que vem com suporte para vários monitores) se `gnome-screenshot` tiver sido instalada anteriormente. Para Wayland, `grim` e `slurp` será usado quando ambos estiverem disponíveis. Observe que `gnome-screenshot` não é compatível com compositores Wayland baseados em wlroots. Como `gnome-screenshot` será preferido quando disponível, talvez seja necessário definir a variável de ambiente `SCREENSHOT_TOOL` como `grim` neste caso (outros valores disponíveis são `gnome-screenshot` e `pil`).
+# 
+# - Se o modelo não tiver certeza sobre o que está na imagem, ele poderá gerar uma previsão diferente toda vez que você clicar em `"Tentar novamente"`. Com o parâmetro `temperature` você pode controlar esse comportamento (baixa temperatura produzirá o mesmo resultado).
 # 
 
-# 6.3 Você pode usar uma API. Isso tem dependências adicionais. Instale `pip install -U "pix2tex[api]"` e execute: `python -m pix2tex.api.run`
+# ### Application Program Interface (API)
 # 
-# Para iniciar uma demonstração Streamlit que se conecta à API na porta 8502. Há também uma imagem docker disponível para a API: https://hub.docker.com/r/lukasblecher/pix2tex Tamanho da imagem Docker (mais recente por data)
+# 1. Você pode usar uma API. Isso tem dependências adicionais. Instale `pip install -U "pix2tex[api]"` e execute: `python -m pix2tex.api.run`
+# 
+# Para iniciar uma demonstração Streamlit que se conecta à API na porta 8502. Há também uma imagem `docker` disponível para a API: <https://hub.docker.com/r/lukasblecher/pix2tex> Tamanho da imagem Docker (mais recente por data)
 # 
 # ```
 # docker pull lukasblecher/pix2tex:api
@@ -120,7 +128,9 @@
 # Para executar também a demonstração `streamlit`, execute `docker run --rm -it -p 8501:8501 --entrypoint python lukasblecher/pix2tex:api pix2tex/api/run.py` e navegue até <http://localhost:8501/>
 # 
 
-# 6.4 Use de dentro do `Python`:
+# ### Dentro do `Python`
+# 
+# 1. Use de dentro do `Python`:
 # 
 # Para usar dentro do `Python`, utilize os comandos abaixo:
 # 
